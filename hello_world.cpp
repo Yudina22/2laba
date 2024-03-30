380f1406e1681b5cee0fe9d116cc26f7f9a56d55
Yudina22 <yudina.anastasiyu28@gmail.com>
2024-03-30 22:01:31 +0700

    updated cpp x2

--- hello_world.cpp
+++ hello_world.cpp
@@ -1,8 +1,11 @@
 #include <iostream>
 
+using namespace std;
+
 int main()
+
 {
-cout<<"Vvedite svoe imya"<<endl;
+cout <<"Vvedite svoe imya"<<endl;
 cin>>n>>endl;
-cout<<"Hello world from"<<n<<"!"<<endl;
+cout<<"Helo world from"<<n<<"1"<<endl;
 }
